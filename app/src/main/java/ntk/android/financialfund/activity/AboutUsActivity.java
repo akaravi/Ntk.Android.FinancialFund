8fe7e3778d85bf97acaf96b60c83bb2e9991d40a
ixiflutter1313 <mj.parishani@outlook.com>
2021-01-23 15:04:31 +0330

    Update AboutUsActivity.java

--- app/src/main/java/ntk/android/financialfund/activity/AboutUsActivity.java
+++ app/src/main/java/ntk/android/financialfund/activity/AboutUsActivity.java
@@ -1,6 +1,7 @@
 package ntk.android.financialfund.activity;
 
+import ntk.android.base.activity.common.BaseAboutActivity;
 import ntk.android.base.activity.ticketing.TicketListActivity;
 
-public class AboutUsActivity extends TicketListActivity {
+public class AboutUsActivity extends BaseAboutActivity {
 }
