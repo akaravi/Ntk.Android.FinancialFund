a04fae098a25df6d248e27e2378a29ca025f9099
ixiflutter1313 <mj.parishani@outlook.com>
2021-02-12 11:06:00 +0330

    change class

--- app/src/main/java/ntk/android/financialfund/activity/ArticleSearchActivity.java
+++ app/src/main/java/ntk/android/financialfund/activity/ArticleSearchActivity.java
@@ -4,14 +4,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import io.reactivex.Observable;
 import java9.util.function.Function;
-import ntk.android.base.activity.abstraction.AbstractionSearchActivity;
+import ntk.android.base.activity.abstraction.AbstractSearchActivity;
 import ntk.android.base.entitymodel.article.ArticleContentModel;
 import ntk.android.base.entitymodel.base.ErrorException;
 import ntk.android.base.entitymodel.base.FilterModel;
 import ntk.android.base.services.article.ArticleContentService;
 import ntk.android.financialfund.adapter.ArticleAdapter;
 
-public class ArticleSearchActivity extends AbstractionSearchActivity<ArticleContentModel> {
+public class ArticleSearchActivity extends AbstractSearchActivity<ArticleContentModel> {
 
     @Override
     protected RecyclerView.Adapter getAdapter() {
